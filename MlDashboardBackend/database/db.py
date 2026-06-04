8098fb106dd74b24f2dec6b4082d50ba9f6982ee
Hari8897 <kanchuahrigopikrishna@gmail.com>
2026-06-04 22:16:11 +0530

    v1.0.0 - Sql connection fixed

--- MlDashboardBackend/database/db.py
+++ MlDashboardBackend/database/db.py
@@ -3,8 +3,7 @@ from sqlalchemy import create_engine, inspect, text
 from sqlalchemy.orm  import sessionmaker, declarative_base
 
 
-DATABASE_URL = "postgresql://postgres:Hari1234@localhost:5432/ml_dashboard_db"
-
+DATABASE_URL = "postgresql+psycopg2://postgres:Hari1234@localhost:5432/ml_dashboard_db"
 engine = create_engine(DATABASE_URL, pool_pre_ping=True, echo=True)
 
 
